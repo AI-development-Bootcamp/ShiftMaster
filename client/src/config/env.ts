5ab0fa54cde034aa8ffc35c61dd3b09ec8b5fec7
woozai <wooozai@gmail.com>
2026-01-18 21:22:27 +0200

    added VITE_ prefix

--- client/src/config/env.ts
+++ client/src/config/env.ts
@@ -4,5 +4,5 @@
 
 export const env = {
   apiUrl: import.meta.env.VITE_API_URL || '',
-  renderDeployUrlClient: import.meta.env.RENDER_DEPLOY_URL_CLIENT || '',
+  renderDeployUrlClient: import.meta.env.VITE_RENDER_DEPLOY_URL_CLIENT || '',
 };
