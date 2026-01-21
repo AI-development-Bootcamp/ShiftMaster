5aae0fe6af723a8c8e6c7ee704b00dc545e24e0e
woozai <wooozai@gmail.com>
2026-01-21 12:08:29 +0200

    remmoevd unsued nav

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import DailyEntryCard, {
   DailyEntry,
